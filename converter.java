072413e82afb46f2b47d70b572b2d273f6d085d7
AnnaKosti <145902090+AnnaKosti@users.noreply.github.com>
2023-09-24 12:07:16 +0000

    conversetion

--- converter.java
+++ converter.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class converter {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the currency rate");
+        double currency = sc.nextDouble();
+        System.out.println("Enter amount of money");
+        int hryvna =sc.nextInt();
+        double amount = hryvna / currency;
+        //double rest = hryvna / currency - amount;
+        System.out.println("You can buy " + amount);
+    }
 }
--- dialog.java
+++ dialog.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
 public class dialog {
+   
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name;
         int age;
-
-        System.out.println(x:"Hello");
+        dialog.main(String[])
+         System.out.println(x: "Hello");
         System.out.println(x:"What's your name?");
         name = scanner.nextLine();
         System.out.println(x:"How old are you?");
         age = scanner.nextInt();
 
-         System.out.println(x:"Nice to meet you, " + name "!");
+         System.out.println(x:"Nice to meet you, " + name + "!");
           System.out.println(x:"I know, you're " + age);
     }
 }
